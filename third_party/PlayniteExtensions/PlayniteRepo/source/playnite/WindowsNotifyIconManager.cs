48d03d213c9d4260bce1a74c742af464a69e11b5
hawkeye116477 <hawkeye116477@gmail.com>
2026-08-19 20:20:40 +0200

    Add missing desktop notifcations

--- third_party/PlayniteExtensions/PlayniteRepo/source/playnite/WindowsNotifyIconManager.cs
+++ third_party/PlayniteExtensions/PlayniteRepo/source/playnite/WindowsNotifyIconManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace Playnite
 {
@@ -7,21 +8,16 @@ namespace Playnite
     {
         public static void Notify(Icon icon, string title, string body, Action clickAction = null)
         {
-            // new ToastContentBuilder()
-            //     .AddText(title)
-            //     .AddText(body)
-            //
-            // var notifyIcon = new NotifyIcon
-            // {
-            //     Icon = icon,
-            //     BalloonTipTitle = title,
-            //     BalloonTipText = body,
-            //     Visible = true
-            // };
-            //
-            // notifyIcon.BalloonTipClicked += (o, ea) => { clickAction?.Invoke(); notifyIcon.Dispose(); };
-            // notifyIcon.BalloonTipClosed += (o, ea) => { notifyIcon.Dispose(); };
-            // notifyIcon.ShowBalloonTip(0); // Windows Vista and up timeout is 5sec by default, only Windows Accessibility Settings can override this
+            var notifyIcon = new NotifyIcon
+            {
+                Icon = icon,
+                BalloonTipTitle = title,
+                BalloonTipText = body,
+                Visible = true
+            };
+            notifyIcon.BalloonTipClicked += (o, ea) => { clickAction?.Invoke(); notifyIcon.Dispose(); };
+            notifyIcon.BalloonTipClosed += (o, ea) => { notifyIcon.Dispose(); };
+            notifyIcon.ShowBalloonTip(60); // Windows Vista and up timeout is 5sec by default, only Windows Accessibility Settings can override this
         }
     }
 }
